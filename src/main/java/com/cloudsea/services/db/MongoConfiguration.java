552e0ffbe5ad5af6c92e77e49f4b4b7495861ad4
Shahbaz <jocnud@gmail.com>
2017-04-09 00:14:57 +0530

    removed spring related dependecies

--- src/main/java/com/cloudsea/services/db/MongoConfiguration.java
+++ src/main/java/com/cloudsea/services/db/MongoConfiguration.java
@@ -1,30 +1,23 @@
 package com.cloudsea.services.db;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
 
 /**
- * A configuration class to create
- * MongoClient
+ * A configuration class to create MongoClient
  * 
- *  Move the Mongo URI to read from
- * properties file.
+ * Move the Mongo URI to read from properties file.
  * 
  * @author shahbaz03
  *
  */
 
-@Configuration
 public class MongoConfiguration {
-	
-	@Bean
-    public MongoClient getMongoClient(){
-		MongoClientOptions mongoOptions = new MongoClientOptions.Builder().connectionsPerHost(8).build();
+	public MongoClient getMongoClient() {
+		MongoClientOptions mongoOptions = new MongoClientOptions.Builder().connectionsPerHost(8)
+				.build();
 		MongoClient client = new MongoClient("localhost:27017", mongoOptions);
 		return client;
-    }
+	}
 
 }
